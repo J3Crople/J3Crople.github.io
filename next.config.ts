2906aac45d6973ee006863cc8f8b8254b71c1cbe
Jordan Cropley <j3crople@uwaterloo.ca>
2025-09-28 17:46:48 -0400

    Added CSS to gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enables static HTML export
-  // basePath and assetPrefix only if using a GitHub repo subpath
-  // basePath: '/my-portfolio',
-  // assetPrefix: '/my-portfolio/',
+  output: 'export',
+  assetPrefix: '/my-portfolio/',
 };
 
 module.exports = nextConfig;
