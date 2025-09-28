dd5881046605edd3228f6483402a35362a683e4d
Jordan Cropley <j3crople@uwaterloo.ca>
2025-09-28 18:01:36 -0400

    Changed files

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   assetPrefix: '/J3crople.github.io/',
   images: {
-    unoptimized: true,        // Avoids Next.js image optimization errors on static export
+    unoptimized: true,
   },
 };
 
