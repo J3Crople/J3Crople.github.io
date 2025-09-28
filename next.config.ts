013fd7b20b45393d070bde60d97aceac0c5ef2e7
Jordan Cropley <j3crople@uwaterloo.ca>
2025-09-28 17:43:49 -0400

    Added gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // enables static HTML export
+  // basePath and assetPrefix only if using a GitHub repo subpath
+  // basePath: '/my-portfolio',
+  // assetPrefix: '/my-portfolio/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
