4b85bbaa676548d77dd091e9ebf71ad8aaeac322
Jordan Cropley <j3crople@uwaterloo.ca>
2025-09-28 17:55:14 -0400

    Updates

---
+++ _next/static/gyuof-LxCb-cK2tryza5p/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/76ba7675301589b3.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/J3crople.github.io//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
